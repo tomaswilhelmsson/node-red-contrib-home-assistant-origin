00bb51a3bebf32ddb857da97fb779050cbd316d2
Tomas Wilhelmsson <tomas.wilhelmsson@gmail.com>
2024-02-01 23:18:34 +0100

    added messages

--- home-assistant-origin/home-assistant-origin.js
+++ home-assistant-origin/home-assistant-origin.js
@@ -13,21 +13,46 @@ module.exports = function(RED) {
                 return;
             }
 
-            var context = findByKey(msg.data, 'context');
-            node.log(context);
+            var context = null;
+            if(msg.data.hasOwnProperty("context"))
+                context = {
+                    topic: "sensor",
+                    payload: msg.data.context
+                }
+            else if(msg.data.hasOwnProperty("old_state") && msg.data.hasOwnProperty("new_state")) {
+                context = {
+                    topic: "state_change",
+                    payload: msg.data.new_state.context
+                }
+            }
 
-            msg.payload = msg.payload.toLowerCase();
-            node.send([msg, msg, msg, { payload: false }]);
+            if(context.topic === "sensor" || context.topic == "state_change") {
+                if(context.payload.id != null && context.payload.parent_id == null && context.payload.user_id == null)
+                    node.send([msg, null, null, { payload: false }])
+                else if(context.payload.id != null && context.payload.parent_id == null && context.payload.user_id != null)
+                    node.send([null, msg, null, { payload: false }])
+                else if(context.payload.id != null && context.payload.parent_id != null && context.payload.user_id == null)
+                    node.send([null, null, msg, { payload: false}])
+            } else {
+                node.send([null, null, null, { payload: true }]);
+            }
         });
-    }
 
-    function findByKey(obj, kee) {
-        if (kee in obj) return obj[kee];
-        for(n of Object.values(obj).filter(Boolean).filter(v => typeof v === 'object')) {
-            let found = findByKey(n, kee)
-            if (found) return found
+        
+        function findByKey(obj, kee) {
+            let found_objects = [];
+            if (kee in obj) return [obj[kee]];
+//            for(const [key, n] of Object.entries(obj).filter(Boolean).filter(v => typeof v === 'object')) {
+            node.warn(obj);
+            for(n of Object.values(obj).filter(Boolean).filter(v => typeof v === 'object')) {
+                node.warn(n);
+                let found = findByKey(n, kee)
+                if (found) found_objects.push(found);
+            }
+            return found_objects;
         }
     }
+
 
     RED.nodes.registerType("home-assistant-origin",HomeAssistantOriginNode);
 }
